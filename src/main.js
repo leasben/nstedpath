b2917e83d981d56fbd6f5f3e0896348003fc3ffd
leasben <simbaleee123@gmail.com>
2026-02-18 13:07:48 +0200

    Rosa Page left

--- src/main.js
+++ src/main.js
@@ -9,3 +9,19 @@ jQuery(function ($) {
       $('#mobile_menu').toggle();
   });
 });
+
+/* BACK TO TOP */
+$(document).ready(function() {
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+          $('#backToTop').fadeIn();
+      } else {
+          $('#backToTop').fadeOut();
+      }
+  });
+
+  $('#backToTop').on('click', function() {
+      $('html, body').animate({ scrollTop: 0 }, 120, 'swing'); 
+      return false;
+  });
+})
